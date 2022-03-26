c7d2d4c7be3348ad1906eb5637d0c795784e01c2
Luis Cano <luecano@gmail.com>
2022-03-26 17:35:58 -0500

    wip product crud

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,20 +1,51 @@
-function getProducts(req, res) {
-    res.render('admin/products/all-products');
+const Product = require('../models/product.model');
+
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll();
+        res.render('admin/products/all-products', { products: products });
+    } catch (error) {
+        return next(error);
+    }
 }
 
 function getNewProduct(req, res) {
     res.render('admin/products/new-product');
 }
 
-function createNewProduct(req, res) {
-    console.log(req.body);
-    console.log(req.file);
+async function createNewProduct(req, res, next) {
+    const product = new Product({
+        ...req.body,
+        image: req.file.filename
+    });
+
+    try {
+        await product.save();
+    } catch (error) {
+        next(error);
+        return;
+    }
 
     res.redirect('/admin/products');
+}
+
+async function getUpdateProduct(req, res, next) {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.render('admin/products/update-product', { product: product });
+    } catch (error) {
+        next('error');
+    }
+}
+
+function updateProduct() {
+
 }
 
 module.exports = {
     getProducts: getProducts,
     getNewProduct: getNewProduct,
-    createNewProduct: createNewProduct
+    createNewProduct: createNewProduct,
+    getUpdateProduct: getUpdateProduct,
+    updateProduct: updateProduct
 }
