5012f110b02f7ca5e66f3177dd5850e6fa04e133
Luis Cano <luecano@gmail.com>
2022-04-03 10:34:22 -0500

    add stripe payment

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,5 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
@@ -33,10 +35,40 @@ async function addOrder(req, res, next) {
 
     req.session.cart = null;
 
-    res.redirect('/orders');
+    const session = await stripe.checkout.sessions.create({
+        line_items: cart.items.map(function (item) {
+            return {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: item.product.title
+                    },
+                    unit_amount: +item.product.price.toFixed(2) * 100
+                },
+                quantity: item.quantity,
+            }
+        }),
+        mode: 'payment',
+        success_url: `http://localhost:3000/orders/success`,
+        cancel_url: `http://localhost:3000/orders/error`,
+    });
+
+    res.redirect(303, session.url);
+    // res.redirect('/orders');
+}
+
+function getSuccess(req, res) {
+    res.render('customer/orders/success');
+}
+
+function getError(req, res) {
+    res.render('customer/orders/error');
 }
 
 module.exports = {
     addOrder: addOrder,
     getOrders: getOrders,
+    getSuccess: getSuccess,
+    getError: getError
 };
