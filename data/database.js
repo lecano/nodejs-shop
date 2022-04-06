f8b5c5b5fd586d5f4d66495ff8ec343b91e47afc
Luis Cano <luecano@gmail.com>
2022-04-05 20:52:08 -0500

    deployment

--- data/database.js
+++ data/database.js
@@ -2,10 +2,15 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let mongoURL = 'mongo://localhost:27017';
+if (process.env.MONGO_URL) {
+    mongoURL = process.env.MONGO_URL;
+}
+
 let database;
 
 async function connectToDatabase() {
-    const client = await MongoClient.connect(process.env.MONGO_URL);
+    const client = await MongoClient.connect(mongoURL);
     database = client.db('online-shop')
 }
 
